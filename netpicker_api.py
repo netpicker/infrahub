c959350fb1a69715447d2bbef9cf8277ca26ee90
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-09-11 11:59:39 +0000

    Print OK

--- netpicker_api.py
+++ netpicker_api.py
@@ -4,14 +4,15 @@ from infrahub_sdk.checks import InfrahubCheck
 
 class Check(InfrahubCheck):
     def validate(self, data):
-        url = "https://sandbox.netpicker.io/api/v1/auth/info"
+        # url = "https://sandbox.netpicker.io/api/v1/auth/info"
         
-        try:
-            response = requests.get(url)
-            # response.raise_for_status()
+        # try:
+        #     response = requests.get(url)
+        #     # response.raise_for_status()
             
-            print("Response from auth/info:")
-            print(response.json())
+        #     print("Response from auth/info:")
+        #     print(response.json())
         
-        except requests.RequestException as e:
-            print(f"An error occurred: {e}")
+        # except requests.RequestException as e:
+        #     print(f"An error occurred: {e}")
+        print('OK')
