5d60b4d94dcf860ada6252e1f4107fe69369f7fa
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-09-11 13:55:36 +0000

    New test

---
+++ netpicker_api.py
@@ -0,0 +1,19 @@
+import requests
+from infrahub_sdk.checks import InfrahubCheck
+
+
+class Check(InfrahubCheck):
+    query = "tags_check"
+
+    def validate(self, data):
+        url = "https://sandbox.netpicker.io/api/v1/auth/info"
+
+        try:
+            response = requests.get(url)
+            # response.raise_for_status()
+            
+            print("Response from auth/info:")
+            print(response.json())
+        
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
