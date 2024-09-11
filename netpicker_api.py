8e7ec33d117b8d7467a1e676d5491c4484f28474
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-09-11 12:16:15 +0000

    Test error

--- netpicker_api.py
+++ netpicker_api.py
@@ -17,4 +17,6 @@ class Check(InfrahubCheck):
         
         # except requests.RequestException as e:
         #     print(f"An error occurred: {e}")
-        print('OK')
+        self.log_error(
+                    message=f"Test error"
+                )
