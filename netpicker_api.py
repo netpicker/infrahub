78f2cbb2f7e3caefe9c05e662269597c81e138dc
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-09-16 15:46:51 +0200

    Update netpicker_api.py

--- netpicker_api.py
+++ netpicker_api.py
@@ -39,6 +39,38 @@ class Check(InfrahubCheck):
             
             self.log_info(message="Response from auth/info:")
             self.log_info(message=info_response.json())
+
+            # Debug request
+            debug_url = "http://opsmill-netpicker.tailc018d.ts.net/api/v1/policy/default/Infrahub_SDK/debug"
+            debug_headers = {
+                'Accept': 'application/json, text/plain, */*',
+                'Authorization': f'Bearer {access_token}',
+                'Content-Type': 'application/json',
+            }
+            python_code = """@medium(
+    name='rule_show_interfaces',
+)
+def rule_show_interfaces(configuration, commands, device):
+    assert configuration == False, 'Error message'
+"""
+
+            debug_data = {
+                "name": "rule_show_interfaces",
+                "severity": "MEDIUM",
+                "configuration": "example config here",
+                #"ipaddress": "cisco_ios",
+                "command": None,
+                "ruleset": "default",
+                "definition": {
+                    "code": python_code
+                }
+            }
+
+            debug_response = requests.post(debug_url, headers=debug_headers, json=debug_data, verify=False)
+            debug_response.raise_for_status()
+
+            self.log_info(message="Response from debug endpoint:")
+            self.log_info(message=debug_response.json())
         
         except requests.RequestException as e:
             self.log_error(message=f"An error occurred: {e}")
