55793021714b3297f65fb6ff52b73fc3bbf8cad2
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-10-16 11:11:33 +0200

    Clean up

--- print_ok.py
+++
@@ -1,9 +0,0 @@
-import requests
-from infrahub_sdk.checks import InfrahubCheck
-
-
-class PrintOKCheck(InfrahubCheck):
-    query = "tags_check"
-
-    def validate(self, data):
-        print('OK!')
